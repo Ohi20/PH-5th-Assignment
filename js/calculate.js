25c6529a5ac3413d07aced808d8e2a92c7950677
Ohi97 <ashrafohi20@gmail.com>
2022-02-17 13:35:51 +0600

    second-commit

---
+++ js/calculate.js
@@ -0,0 +1,88 @@
+document.getElementById("calculate-button").addEventListener('click',function(){
+    
+    // getting expenses input values
+    const foodInput = document.getElementById("food-input");
+   const foodMoney = foodInput.value;
+    const rentInput = document.getElementById("rent-input");
+   const rentMoney = rentInput.value;
+    const clothesInput = document.getElementById("clothes-input");
+   const clothesMoney = clothesInput.value;
+
+   const totalExpenses = parseFloat(foodMoney) + parseFloat(rentMoney) + parseFloat(clothesMoney);
+    
+    //getting current total expense value
+    const expensesTotal = document.getElementById("expenses-total");
+    const expensesTotalText = expensesTotal.innerText;
+    expensesTotal.innerText = parseFloat(totalExpenses);
+    
+    // clear input field
+    foodInput.value = '';
+    rentInput.value = '';
+    clothesInput.value = '';
+
+    // get imcome value
+    
+        const incomeInput = document.getElementById("income-input");
+        const incomeMoney = incomeInput.value;
+            
+    
+    
+
+    // get current balance value
+    
+     const balanceTotal = document.getElementById("balance-total");
+     const balanceTotalText = balanceTotal.innerText;
+     balanceTotal.innerText = incomeMoney - parseFloat(totalExpenses);
+
+    //  clear income input
+    incomeInput.value = "";
+})
+
+document.getElementById("save-button").addEventListener('click',function(){
+    // get income value
+    const incomeInput = document.getElementById("income-input");
+    const incomeMoney = incomeInput.value;
+
+    // get expenses values
+    const foodInput = document.getElementById("food-input");
+    const foodMoney = foodInput.value;
+     const rentInput = document.getElementById("rent-input");
+    const rentMoney = rentInput.value;
+     const clothesInput = document.getElementById("clothes-input");
+    const clothesMoney = clothesInput.value;
+
+    // total expenses
+    const totalExpenses = parseFloat(foodMoney) + parseFloat(rentMoney) + parseFloat(clothesMoney);
+
+    //get current total expense value
+    const expensesTotal = document.getElementById("expenses-total");
+    const expensesTotalText = expensesTotal.innerText;
+    expensesTotal.innerText = parseFloat(totalExpenses);
+
+    // get balance
+
+    const balanceTotal = document.getElementById("balance-total");
+     const balanceTotalText = balanceTotal.innerText;
+     balanceTotal.innerText = incomeMoney - parseFloat(totalExpenses);
+
+    //  find Saving
+    const bTotal = balanceTotal.innerText;
+     const save = bTotal * (20/100);
+    // Saving amount
+     const totalSaving = document.getElementById("total-saving");
+     const totalSavingText = totalSaving.innerText;
+     totalSaving.innerText = save;
+
+    //  remaining balance
+     const remainingBalance = document.getElementById("remaining-balance");
+     const remainingBalanceText = remainingBalance.innerText;
+     remainingBalance.innerText = bTotal - save;
+
+    //  clear input field
+    foodInput.value = '';
+    rentInput.value = '';
+    clothesInput.value = '';
+    incomeInput.value = '';
+  
+   
+})
